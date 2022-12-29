ff57984540131f76e17ef5b2fdc7976113b568dd
Javier Vallejo <rjavier001@gmail.com>
2022-12-29 11:04:15 -0500

    bug fix

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Card from '../components/Card'
-import Footer from '../components/Footer'
-import NavBar from '../components/NavBar'
+
 
 const AboutPage = () => {
   return (
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Counter from '../components/Counter'
-import Footer from '../components/Footer'
-import NavBar from '../components/NavBar'
+
 
 const ContactPage = () => {
   return (
